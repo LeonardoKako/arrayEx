59ad0f87779117ce092c82699de38520b75753ba
LeonardoKako <fleithkako@gmail.com>
2024-11-07 10:59:56 -0300

    .

---
+++ main.js
@@ -0,0 +1,14 @@
+let alunos = [
+    {nome: 'leo', nota: 10},
+    {nome: 'gabriel', nota: 8},
+    {nome: 'pedro', nota: 6},
+    {nome: 'nicolas', nota: 4},
+    {nome: 'caio', nota: 2},
+    {nome: 'joao', nota: 0},
+];
+
+let aprovados = alunos.filter(function(aluno) {
+    return aluno.nota >= 7;
+});
+
+console.log(aprovados);
